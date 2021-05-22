81b9a5050bfc3fed7db127497b7fcff90ecdd88f
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-22 17:26:09 +0200

    Update rqt.sql

--- REQ/rqt.sql
+++ REQ/rqt.sql
@@ -1,13 +1,21 @@
+\! echo "le nom des produis et leur prix actuel"
 SELECT nom, date, prix FROM evolution_prix AS ev ,produits as pr where pr.id_produit=ev.id_produit and date=(select max("date") from evolution_prix as evo  where pr.id_produit=evo.id_produit);
 
+\! echo "le nom des produis et leur prix actuel mais qui ne sont plus disponible"
 SELECT nom, date, prix FROM evolution_prix AS ev ,(select * from produits where dispo='f') as pr where pr.id_produit=ev.id_produit and date=(select max("date") from evolution_prix as evo  where pr.id_produit=evo.id_produit);
 
-select id_produit ,count("id_produit") from refus group by id_produit order by count desc limit 10;
+\! echo "les id_produit des 20 produits qui ont etait refusé le plus"
+select id_produit ,count("id_produit") from refus group by id_produit order by count desc limit 20;
 
-select count(id_commande) ,nom , l.id_client from commandes as c , clients as l where l.id_client=c.id_client group by l.id_client order by count desc limit 10
+\! echo "les 10 clients qui on fait le plus de commande"
+select count(id_commande) ,nom , l.id_client from commandes as c , clients as l where l.id_client=c.id_client group by l.id_client order by count desc limit 10;
 
-select nom ,taille ,poids ,matiere,couleur from produits as p , equipements as e ,produit_categories as pc where p.id_produit=pc.id_produit and e.id_categorie=pc.id_categorie and p.id_produit=1101;
+\! echo "le nom le poids la matiere et la couleur des composants du produit numero 1101"
+select nom  ,poids ,matiere,couleur from produits as p , equipements as e ,produit_categories as pc where p.id_produit=pc.id_produit and e.id_categorie=pc.id_categorie and p.id_produit=1101;
 
-select c.nom,c.id_client, p.nom,r.motif from clients as c , commandes as co ,produits as p, retours as r where c.id_client=co.id_client and r.id_commande=co.id_commande and p.id_produit=r.id_produit group by c.id_client, p.nom, r.motif;
+\! echo "10 retours avec l'id et le nom du client qui a fait le retour aussi le nom du produit et le motif du retour"
+select c.nom,c.id_client, p.nom,r.motif from clients as c , commandes as co ,produits as p, retours as r where c.id_client=co.id_client and r.id_commande=co.id_commande and p.id_produit=r.id_produit group by c.id_client, p.nom, r.motif limit 10;
 
+\! echo "pour chaque client qui a donné des avis le nombre d'etoile classé par étoile"
+select c.id_client,c.nom ,c.prenom ,etoile, count(etoile) from clients as c , avis as a where a.id_client=c.id_client  group by c.id_client,etoile;
 
