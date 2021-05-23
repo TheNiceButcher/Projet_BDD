18cd4947db4e9fa9a99d00ffe3f67690f5dedd89
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-23 12:35:27 +0200

    Update rqt.sql

--- REQ/rqt.sql
+++ REQ/rqt.sql
@@ -19,3 +19,15 @@ select c.nom,c.id_client, p.nom,r.motif from clients as c , commandes as co ,pro
 \! echo "pour chaque client qui a donné des avis le nombre d'etoile classé par étoile"
 select c.id_client,c.nom ,c.prenom ,etoile, count(etoile) from clients as c , avis as a where a.id_client=c.id_client  group by c.id_client,etoile;
 
+\! echo "les 10 clients qui on dépensé le plus d'argent sur notre magasin son compter les annulations"
+select c.nom, sum(prix) from clients as c ,commandes as co, produit_commandé as pc ,evolution_prix as ev 
+where c.id_client=co.id_client and co.id_commande=pc.id_commande and pc.annulation='f' and pc.id_produit=ev.id_produit and co.date=(
+select max(date) from evolution_prix as ev1 where ev1.id_produit=pc.id_produit and ev1.date<= co.date) 
+group by c.id_client order by sum desc limit 10;
+
+\! echo "les 10 clients qui on dépensé le plus d'argent sur notre magasin son compter les refus ou les retour et les annulations"
+select c.nom, sum(prix) from clients as c ,commandes as co, produit_commandé as pc ,evolution_prix as ev 
+where c.id_client=co.id_client and co.id_commande=pc.id_commande and pc.annulation='f' and pc.id_produit=ev.id_produit and co.date=(
+select max(date) from evolution_prix as ev1 where ev1.id_produit=pc.id_produit and ev1.date<= co.date) and (pc.id_commande, pc.id_produit,pc.id_colis,pc.exemplaire)
+NOT IN ( (select r.id_commande,r.id_produit,r.id_colis,r.exemplaire from retours as r) UNION (select r.id_commande,r.id_produit,r.id_colis,r.exemplaire from refus as r) )    
+group by c.id_client order by sum desc limit 10;
