68a7159c3f15293e96916e57f47169c5bb5c9ca4
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-24 23:15:47 +0200

    Update rqt.sql

--- REQ/rqt.sql
+++ REQ/rqt.sql
@@ -63,4 +63,17 @@ from produit_commandé as p , produit_de_x as c, commandes as co
 where c.id_client=co.id_client and co.id_commande=p.id_commande)
 select * from produit_de_x where id_produit<>0;
 
+\! echo "le produit qui a été commandé sur tous les commandes avec de l’agrégation"
+SELECT p.id_produit from produit_commandé as p group by id_produit having count(distinct id_commande)=(select count(distinct id_commande) from commandes );
+
+\! echo "le produit qui a été commandé sur tous les commandes avec des sous requetes corrélées --attention cette requette peut prendre des minutes pour calculer le resultat vu la taille des deux tables dans le NUTURAL JOIN--"
+SELECT p.id_produit from produit_commandé as p where NOT EXISTS(
+SELECT c.id_commande from commandes as c where c.id_commande NOT IN
+(select c2.id_commande from commandes as c2 NATURAL JOIN produit_commandé as p2 
+where p2.id_produit=p.id_produit));
+
+
+
+
+
 
