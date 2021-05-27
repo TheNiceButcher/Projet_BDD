ef2ebf5214a5381ddd08fd6ef5c896bd9a107af9
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-27 22:26:48 +0200

    Add new file

---
+++ REQ/req19.sql
@@ -0,0 +1,3 @@
+\! echo "les produits et leurs avis avec id du client qui a donnée l avis et NULL pour les produis qui ont jamais eu d avis"
+
+SELECT avis.id_client, produits.id_produit, avis FROM produits LEFT JOIN avis ON avis.id_produit=produits.id_produit ;
