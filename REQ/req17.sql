97dd25b5e60a08a78c65b94dae498e54d3218b92
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-27 22:25:01 +0200

    Add new file

---
+++ REQ/req17.sql
@@ -0,0 +1,4 @@
+\! echo "les 10 produits qui on été evaluer le plus deux requetes qui donnent la meme chose si la table ne contient pas des null et le contarire si la table contient des null"
+
+\! echo "avec count(*)"
+select id_produit ,count(*) from avis group by id_produit order by count desc limit 10;
