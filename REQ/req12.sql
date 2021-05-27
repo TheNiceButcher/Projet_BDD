8e34f1066b9148fc8c1f5934a45a067ec7cba591
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-27 23:24:29 +0200

    Update req12.sql

--- REQ/req12.sql
+++ REQ/req12.sql
@@ -1,7 +1,8 @@
 \! echo "12- les 10 produits les mieux noté "
 
-select nom, avg(etoile)
+select nom, avg(etoile),count(etoile) 
 from produits as p join avis as a on (a.id_produit=p.id_produit)
-group by p.id_produit
-order by avg desc
+group by (p.id_produit,etoile) 
+having etoile IS NOT NULL 
+order by (avg) desc 
 limit 10;
