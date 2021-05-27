6dc0d23f07e15a1f72934985203d250aac30f8e7
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-27 21:13:46 +0200

    Update rqt.sql

--- REQ/rqt.sql
+++ REQ/rqt.sql
@@ -13,26 +13,26 @@ select count(id_commande) ,nom , l.id_client from commandes as c , clients as l 
 \! echo "le nom le poids la matiere et la couleur des composants du produit numero 1101"
 select nom  ,poids ,matiere,couleur from produits as p , equipements as e ,produit_categories as pc where p.id_produit=pc.id_produit and e.id_categorie=pc.id_categorie and p.id_produit=1101;
 
-\! echo "10 retours avec l'id et le nom du client qui a fait le retour aussi le nom du produit et le motif du retour"
+\! echo "10 retours avec  id et le nom du client qui a fait le retour aussi le nom du produit et le motif du retour"
 select c.nom,c.id_client, p.nom,r.motif from clients as c , commandes as co ,produits as p, retours as r where c.id_client=co.id_client and r.id_commande=co.id_commande and p.id_produit=r.id_produit group by c.id_client, p.nom, r.motif limit 10;
 
-\! echo "pour chaque client qui a donné des avis le nombre d'etoile classé par étoile"
+\! echo "pour chaque client qui a donné des avis le nombre d etoile classé par étoile"
 select c.id_client,c.nom ,c.prenom ,etoile, count(etoile) from clients as c , avis as a where a.id_client=c.id_client  group by c.id_client,etoile;
 
-\! echo "les 10 clients qui on dépensé le plus d'argent sur notre magasin son compter les annulations"
+\! echo "les 10 clients qui on dépensé le plus d argent sur notre magasin son compter les annulations"
 select c.nom, sum(prix) from clients as c ,commandes as co, produit_commandé as pc ,evolution_prix as ev 
 where c.id_client=co.id_client and co.id_commande=pc.id_commande and pc.annulation='f' and pc.id_produit=ev.id_produit and co.date=(
 select max(date) from evolution_prix as ev1 where ev1.id_produit=pc.id_produit and ev1.date<= co.date) 
 group by c.id_client order by sum desc limit 10;
 
-\! echo "les 10 clients qui on dépensé le plus d'argent sur notre magasin son compter les refus ou les retour et les annulations"
+\! echo "les 10 clients qui on dépensé le plus d argent sur notre magasin son compter les refus ou les retour et les annulations"
 select c.nom, sum(prix) from clients as c ,commandes as co, produit_commandé as pc ,evolution_prix as ev 
 where c.id_client=co.id_client and co.id_commande=pc.id_commande and pc.annulation='f' and pc.id_produit=ev.id_produit and co.date=(
 select max(date) from evolution_prix as ev1 where ev1.id_produit=pc.id_produit and ev1.date<= co.date) and (pc.id_commande, pc.id_produit,pc.id_colis,pc.exemplaire)
 NOT IN ( (select r.id_commande,r.id_produit,r.id_colis,r.exemplaire from retours as r) UNION (select r.id_commande,r.id_produit,r.id_colis,r.exemplaire from refus as r) )    
 group by c.id_client order by sum desc limit 10;
 
-\! echo "le chifre d'affaire de notre magasin sans compter les retour et les refus"
+\! echo "le chifre d affaire de notre magasin sans compter les retour et les refus"
 select sum(prix) from clients as c ,commandes as co, produit_commandé as pc ,evolution_prix as ev
 where c.id_client=co.id_client and co.id_commande=pc.id_commande and pc.annulation='f' and pc.id_produit=ev.id_produit and co.date=(
 select max(date) from evolution_prix as ev1 
@@ -63,16 +63,16 @@ from produit_commandé as p , produit_de_x as c, commandes as co
 where c.id_client=co.id_client and co.id_commande=p.id_commande)
 select * from produit_de_x where id_produit<>0;
 
-\! echo "le produit qui a été commandé sur tous les commandes avec de l’agrégation"
+\! echo "le produit qui a été commandé sur tous les commandes avec de l agrégation"
 SELECT p.id_produit from produit_commandé as p group by id_produit having count(distinct id_commande)=(select count(distinct id_commande) from commandes );
 
-\! echo "le produit qui a été commandé sur tous les commandes avec des sous requetes corrélées --attention cette requette peut prendre des minutes pour calculer le resultat vu la taille des deux tables dans le NUTURAL JOIN--"
+\! echo "le produit qui a été commandé sur tous les commandes avec des sous requetes corrélées attention cette requette peut prendre des minutes pour calculer le resultat vu la taille des deux tables dans le NUTURAL JOIN"
 SELECT p.id_produit from produit_commandé as p where NOT EXISTS(
 SELECT c.id_commande from commandes as c where c.id_commande NOT IN
 (select c2.id_commande from commandes as c2 NATURAL JOIN produit_commandé as p2 
 where p2.id_produit=p.id_produit));
 
-\! echo "les 10 produits qui on été evaluer le plus --deux requetes qui donnent la meme chose si la table ne contient pas des null et le contarire si la table contient des null"
+\! echo "les 10 produits qui on été evaluer le plus deux requetes qui donnent la meme chose si la table ne contient pas des null et le contarire si la table contient des null"
 
 \! echo "avec count(*)"
 select id_produit ,count(*) from avis group by id_produit order by count desc limit 10;
@@ -80,8 +80,11 @@ select id_produit ,count(*) from avis group by id_produit order by count desc li
 \! echo "avec count(etoile) et etoile est un attribut nullable"
 select id_produit ,count(etoile) from avis group by id_produit order by count desc limit 10;
 
-\! echo "les produits et leurs avis avec id du client qui a donnée l'avis et NULL pour les produis qui ont jamais eu d'avis"
+\! echo "les produits et leurs avis avec id du client qui a donnée l avis et NULL pour les produis qui ont jamais eu d avis"
 SELECT avis.id_client, produits.id_produit, avis FROM produits LEFT JOIN avis ON avis.id_produit=produits.id_produit ;
+
+\! echo "les commandes qui ont eu plus de un produit refusé"
+SELECT distinct(r.id_commande) FROM refus as r, refus as r1 WHERE r.id_commande=r1.id_commande AND (r.exemplaire<>r1.exemplaire);
 
 
 
