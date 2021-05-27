265eed14ed1dcef3e74292e4c92b2fdf0de1249b
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-27 22:08:15 +0200

    Add new file

---
+++ REQ/req2.sql
@@ -0,0 +1,2 @@
+\! echo "le nom des produis et leur prix actuel mais qui ne sont plus disponible"
+SELECT nom, date, prix FROM evolution_prix AS ev ,(select * from produits where dispo='f') as pr where pr.id_produit=ev.id_produit and date=(select max("date") from evolution_prix as evo  where pr.id_produit=evo.id_produit);
