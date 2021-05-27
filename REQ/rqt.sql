50e13a988dc3e63e2bf861a101465db06da52126
DJELLOUL ABBOU mohammed adel <adel0409@hotmail.com>
2021-05-27 18:15:43 +0200

    Update rqt.sql

--- REQ/rqt.sql
+++ REQ/rqt.sql
@@ -80,5 +80,8 @@ select id_produit ,count(*) from avis group by id_produit order by count desc li
 \! echo "avec count(etoile) et etoile est un attribut nullable"
 select id_produit ,count(etoile) from avis group by id_produit order by count desc limit 10;
 
+\! echo "les produits et leurs avis avec id du client qui a donnée l'avis bien sur NULL pour les produis qui ont jamais eu d'avis"
+SELECT avis.id_client, produits.id_produit, avis FROM produits LEFT JOIN avis ON avis.id_produit=produits.id_produit ;
+
 
 
